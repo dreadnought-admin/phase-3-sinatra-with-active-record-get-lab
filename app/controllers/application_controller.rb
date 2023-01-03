ff801bfa842e10fc2a7e554056b7dbd0214e04c6
dom alexander <dom.alexan@gmail.com>
2023-01-03 14:40:35 -0700

    commit tree f93d88024ce8e9b28fcf1f7c0d455fc5bb0fe1a7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,53 @@
-class ApplicationController < Sinatra::Base
+# class ApplicationController < Sinatra::Base
 
-  # add routes
+#   # add routes
+
+#   get '/bakeries' do
+#     set :default_content_type, 'application/json'
+#     bakeries = Bakery.all
+
+#     bakeries.to_json
+#   end 
+
+#   get '/bakeries/:id' do
+#     set :default_content_type, 'application/json'
+#     bakery = Bakery.find(params[:id])
+#     bakery.to_json(include: { :baked_goods })
+#   end 
+
+#   get '/baked_goods/by_price' do
+#     baked_good = BakedGood.all.order(:price)
+#   end 
+
+#   get 'baked_goods/most_expensive' do
+#     baked_good = BakedGood.all.max(:price).limit(1)
+#   end 
+
+# end
+
+class ApplicationController < Sinatra::Base
+  set default_content_type: "application/json"
+  
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
 
 end
